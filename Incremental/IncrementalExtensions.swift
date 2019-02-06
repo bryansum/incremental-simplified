fd316f3e6a49a5df56c3320fce5c6a8173dd125e
Bryan Summersett <bsummersett@gmail.com>
2019-02-06 08:44:40 -0800

    Add further experimental extensions
    As it turns out, my mental model for this library was entirely incorrect
    -- observers in Incremental land always have a value (although not
    directly accessible). This percludes usage of operators like `filter`,
    etc., as it's an error not to immediately receive a value when calling
    `observe`.
    
    In that sense this library is a more limited version of a more
    traditional reactive library -- but hopefully one that is still useful.

--- Incremental/IncrementalExtensions.swift
+++ Incremental/IncrementalExtensions.swift
@@ -14,7 +14,32 @@ extension I {
     target.strongReferences.add(addReader(reader))
     return target
   }
+
+  public func filterMap<B: Equatable>(_ transform: @escaping (A) -> B?) -> I<B> {
+    return filterMap(eq: ==, transform)
+  }
 }
+
+extension I {
+  public func filter(eq: @escaping (A, A) -> Bool, _ isIncluded: @escaping (A) -> Bool) -> I<A> {
+    let target = I<A>(eq: eq)
+    let reader = AnyReader { [unowned target] in
+      if isIncluded(self.value) {
+        target.write(self.value)
+      }
+      return target
+    }
+    target.strongReferences.add(addReader(reader))
+    return target
+  }
+}
+
+extension I where A: Equatable {
+  public func filter(_ isIncluded: @escaping (A) -> Bool) -> I<A> {
+    return filter(eq: ==, isIncluded)
+  }
+}
+
 
 // MARK: - Optionals
 
